dee1310282323b4ab43170f4ec6450fd80c31a22
Martin Ruderer <martin.ruderer@orbitlabs.de>
2022-12-06 10:08:49 +0100

    Day 6

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,9 @@ use crate::days::day3::solution as s3;
 use crate::days::day4::solution as s4;
 use crate::days::day5::solution1 as s51;
 use crate::days::day5::solution2 as s52;
+use crate::days::day6::solution as s6;
 
 
 fn main() {
-    s1();
-    s2();
-    s3();
-    s4();
-    s51();
-    s52();
+    s6()
 }
