9f345fc76f984bb751b229c2108ac9cb73126b77
Martin Ruderer <martin.ruderer@orbitlabs.de>
2023-02-07 16:31:23 +0100

    Day 24 - scaffold

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ use crate::days::day20::solution as s20;
 use crate::days::day21::solution as s21;
 use crate::days::day22::solution as s22;
 use crate::days::day23::solution as s23;
+use crate::days::day24::solution as s24;
 use crate::days::day3::solution as s3;
 use crate::days::day4::solution as s4;
 use crate::days::day5::solution1 as s51;
@@ -29,5 +30,5 @@ use crate::days::day8::solution as s8;
 use crate::days::day9::solution as s9;
 
 fn main() {
-    s23()
+    s24()
 }
